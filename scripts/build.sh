a4a13c58b29d4cc4609ae43009a0aaec1468b7d9
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2024-10-28 22:34:17 -0700

    chore: update tests to use site-generator-beta
    the site-generator-beta is the new aep website.
    Use that in CI instead of the old one.

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -x
+export AEP_LOCATION="${PWD}"
+export SG_DIRECTORY="/tmp/site-generator-beta"
+export AEP_LINTER_LOC="${SG_DIRECTORY}/api-linter"
+if [ ! -d "${SG_DIRECTORY}" ]; then
+    git clone https://github.com/aep-dev/site-generator-beta.git "${SG_DIRECTORY}"
+fi
+
+if [ ! -d "${AEP_LINTER_LOC}" ]; then
+    git clone https://github.com/aep-dev/api-linter.git "${AEP_LINTER_LOC}"
+fi
+cd "${SG_DIRECTORY}" || exit
+# make rules / website folder
+mkdir -p src/content/docs/tooling/linter/rules
+mkdir -p src/content/docs/tooling/website
+npm install
+npm run generate
---
+++ scripts/serve.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -x
+. ./scripts/build.sh
+npm run preview
